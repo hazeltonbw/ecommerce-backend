2edc0d017135ea8a97d1d42296998bf2b53eee5a
Brandon Hazelton <hazeltonbw@gmail.com>
2023-02-23 00:04:58 -0800

    include mocked data in seeding of database

--- db/seedDatabase.js
+++ db/seedDatabase.js
@@ -14,6 +14,7 @@ const insertOverrideSystemValueIntoQuery = (query) => {
   // on how to add override system value so since the pgp helper insert function
   // just returns a string lets modify it!
   // This will ensure our data gets the same id's in the database everytime
+  //
   const overrideStatement = " OVERRIDING SYSTEM VALUE";
   let insertOverrideStatementAfter = query.indexOf(")");
   return (
@@ -22,20 +23,20 @@ const insertOverrideSystemValueIntoQuery = (query) => {
     query.slice(insertOverrideStatementAfter + 1)
   );
 };
-module.exports = async () => {
+
+// First clear all the tables to ensure we have a clean slate
+const clearUsersTable = `DELETE FROM users`;
+const clearProductsTable = `DELETE FROM products`;
+const clearOrdersTable = `DELETE FROM orders`;
+const clearCategoriesTable = `DELETE FROM categories`;
+const clearCartsTable = `DELETE FROM carts`;
+const clearCartHasProductsTable = `DELETE FROM cart_has_products`;
+const clearOrderHasProductsTable = `DELETE FROM order_has_products`;
+
+const seedDatabase = async () => {
   // Create all the mock data given the mockData class
   const mockDataInstance = new mockData();
 
-  // First clear all the tables to ensure we have a clean slate
-  const clearUsersTable = `DELETE FROM users`;
-  const clearProductsTable = `DELETE FROM products`;
-  const clearOrdersTable = `DELETE FROM orders`;
-  const clearCategoriesTable = `DELETE FROM categories`;
-  const clearCartsTable = `DELETE FROM carts`;
-  const clearCartItemsTable = `DELETE FROM cart_has_products`;
-
-  // mock users
-  const users = mockDataInstance.getMockUsers();
   /* use pg-promise to help streamline the process,
     and to help not repeat ourselves
     this also allows us to easily change how many mock
@@ -49,6 +50,9 @@ module.exports = async () => {
     ColumnSet defines the columns in the table so insert() knows what properties to use
     https://vitaly-t.github.io/pg-promise/helpers.ColumnSet.html
   */
+
+  // mock users
+  const users = mockDataInstance.createMockUsers();
   let cs = new pgp.helpers.ColumnSet(
     ["user_id", "fname", "lname", "email", "password"],
     { table: DB.USERS_TABLE }
@@ -56,14 +60,14 @@ module.exports = async () => {
   let seedUsersTable = pgp.helpers.insert(users, cs);
   seedUsersTable = insertOverrideSystemValueIntoQuery(seedUsersTable);
 
-  const categories = mockDataInstance.getMockCategories();
+  const categories = mockDataInstance.createMockCategories();
   cs = new pgp.helpers.ColumnSet(["category_id", "name"], {
     table: DB.CATEGORIES_TABLE,
   });
   let seedCategoriesTable = pgp.helpers.insert(categories, cs);
   seedCategoriesTable = insertOverrideSystemValueIntoQuery(seedCategoriesTable);
 
-  const products = mockDataInstance.getMockProducts();
+  const products = mockDataInstance.createMockProducts();
   cs = new pgp.helpers.ColumnSet(
     ["product_id", "category_id", "title", "price", "description"],
     {
@@ -73,21 +77,21 @@ module.exports = async () => {
   let seedProductsTable = pgp.helpers.insert(products, cs);
   seedProductsTable = insertOverrideSystemValueIntoQuery(seedProductsTable);
 
-  const orders = mockDataInstance.getMockOrders();
+  const orders = mockDataInstance.createMockOrders();
   cs = new pgp.helpers.ColumnSet(["order_id", "user_id", "date", "status"], {
     table: DB.ORDERS_TABLE,
   });
   let seedOrdersTable = pgp.helpers.insert(orders, cs);
   seedOrdersTable = insertOverrideSystemValueIntoQuery(seedOrdersTable);
 
-  const carts = mockDataInstance.getMockCarts();
+  const carts = mockDataInstance.createMockCarts();
   cs = new pgp.helpers.ColumnSet(["cart_id", "created", "modified"], {
     table: DB.CARTS_TABLE,
   });
   let seedCartsTable = pgp.helpers.insert(carts, cs);
   seedCartsTable = insertOverrideSystemValueIntoQuery(seedCartsTable);
 
-  const cartHasProduct = mockDataInstance.getMockCartHasProducts();
+  const cartHasProduct = mockDataInstance.createMockCartHasProducts();
   cs = new pgp.helpers.ColumnSet(["cart_id", "product_id", "qty"], {
     table: DB.CART_HAS_PRODUCTS_TABLE,
   });
@@ -96,7 +100,7 @@ module.exports = async () => {
     seedCartHasProductsTable
   );
 
-  const orderHasProduct = mockDataInstance.getMockOrderHasProducts();
+  const orderHasProduct = mockDataInstance.createMockOrderHasProducts();
   cs = new pgp.helpers.ColumnSet(["order_id", "product_id", "qty"], {
     table: DB.ORDER_HAS_PRODUCTS_TABLE,
   });
@@ -119,13 +123,14 @@ module.exports = async () => {
     } catch (error) {
       console.error(`There was an issue connecting ${DB.PGDATABASE}: ` + error);
     }
-    //Clear data in tables. That way it can be used to refresh the db.
+    // Clear data in tables. That way it can be used to refresh the db.
     await dbECommerceProjectTest.query(clearUsersTable);
     await dbECommerceProjectTest.query(clearProductsTable);
     await dbECommerceProjectTest.query(clearOrdersTable);
     await dbECommerceProjectTest.query(clearCategoriesTable);
     await dbECommerceProjectTest.query(clearCartsTable);
-    await dbECommerceProjectTest.query(clearCartItemsTable);
+    await dbECommerceProjectTest.query(clearCartHasProductsTable);
+    await dbECommerceProjectTest.query(clearOrderHasProductsTable);
 
     // Seed tables on database
     await dbECommerceProjectTest.query(seedUsersTable);
@@ -141,3 +146,35 @@ module.exports = async () => {
     await dbECommerceProjectTest.end();
   }
 };
+
+const clearDatabase = async () => {
+  const dbECommerceProjectTest = new Client({
+    user: DB.PGUSER,
+    host: DB.PGHOST,
+    database: DB.PGDATABASE,
+    password: DB.PGPASSWORD,
+    port: DB.PGPORT,
+  });
+
+  try {
+    try {
+      await dbECommerceProjectTest.connect();
+    } catch (error) {
+      console.error(`There was an issue connecting ${DB.PGDATABASE}: ` + error);
+    }
+    //Clear data in tables. That way it can be used to refresh the db.
+    await dbECommerceProjectTest.query(clearUsersTable);
+    await dbECommerceProjectTest.query(clearProductsTable);
+    await dbECommerceProjectTest.query(clearOrdersTable);
+    await dbECommerceProjectTest.query(clearCategoriesTable);
+    await dbECommerceProjectTest.query(clearCartsTable);
+    await dbECommerceProjectTest.query(clearCartItemsTable);
+  } catch (err) {
+    console.log("ERROR CLEARING ONE OR MORE TABLES: ", err);
+  } finally {
+    await dbECommerceProjectTest.end();
+  }
+};
+
+module.exports = { seedDatabase, clearDatabase };
+await seedDatabase();
