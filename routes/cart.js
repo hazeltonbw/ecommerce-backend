fa68d4cbf1fd0c6f521408e9ed725b335bbb6013
Brandon Hazelton <hazeltonbw@gmail.com>
2023-02-27 16:23:26 -0800

    add more paths to cart route

--- routes/cart.js
+++ routes/cart.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
+const { isLoggedIn, isAdmin } = require("../middleware/auth");
 
 module.exports = (app) => {
   app.use("/cart", router);
-  router.get("/", cartController.getUsersCart);
+  router.get("/", isLoggedIn, cartController.getUsersCart);
+  router.put("/", isLoggedIn, cartController.editProductInCart);
+  router.delete("/", isLoggedIn, cartController.deleteProductInCart);
+  router.post("/add", isLoggedIn, cartController.addProductToCart);
+  router.get("/admin", isLoggedIn, isAdmin, cartController.getCarts);
 };
