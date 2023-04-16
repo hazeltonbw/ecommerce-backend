1575de2bda988d6bfb093fc6e45ed9589348efc3
Brandon Hazelton <hazeltonbw@gmail.com>
2023-04-16 04:24:18 -0700

    console log all the things, change res.send to res.json

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const dotenv = require("dotenv").config({
 const { PORT } = require("./config");
 
 app.get("/", (req, res, next) => {
+  console.log(req.session, "req.session from root path");
+  console.log(req.passport, "req.passport from root path");
   console.log(req.user, "req.user from root path");
-  res.send(req);
+  console.log(req.session.passport, "req.session.passport from root path");
+  console.log(typeof req.session);
+  res.status(200).json(req.session);
 });
 
 app.listen(PORT, () => {
