28b1dbbbbd4826f8cf2ed7ad886d8c4c1f279ad9
Brandon Hazelton <hazeltonbw@gmail.com>
2023-05-08 01:40:08 -0700

    Provided a more helpful json object for the root page :)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res, next) => {
   if (req.user) {
     res.status(200).json(req.user);
   } else {
-    res.status(200).json({});
+    res.status(200).json("The root says you're not logged in! :)");
   }
 });
 
