60e3ab46d33db4585b3f3f5baeea2abf772c8f82
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 00:57:55 -0700

    Fix error handler, clear cookies on logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,33 +2,22 @@ const userModel = require("../models/user");
 const cartModel = require("../models/cart");
 const { ExpressErrorHandler } = require("../helperFunctions");
 
-const loginPage = async (req, res, next) => {
-  res.send("Redirected to /auth/login! :)");
-};
-
 const registerNewUser = async (req, res, next) => {
   const data = req.body;
 
   try {
     if (!data.password || !data.email) {
-      throw new ExpressErrorHandler(
-        400,
-        "Missing email or password information!"
-      );
+      throw new ExpressErrorHandler(400, "Missing email or password information!")
     }
     const userData = await userModel.registerNewUser(data);
-    if (!userData)
-      throw new new ExpressErrorHandler(500, "User couldn't be created")();
+    if (!userData) {
+      throw new ExpressErrorHandler(500, "User couldn't be created.")
+    }
 
-    const cart = await cartModel.createCart(userData.user_id);
-    const user = { ...userData, cart };
+    // Create the users cart
+    await cartModel.createCart(userData.user_id);
+    const user = { ...userData };
 
-    req.login(user, (err) => {
-      console.log("Registration successful.. Logging in user...");
-      if (err) {
-        return next(err);
-      }
-    });
     res.status(200).send(user);
   } catch (err) {
     next(err);
@@ -41,18 +30,12 @@ const logout = (req, res, next) => {
     if (req.session) {
       req.session.destroy();
     }
+    res.clearCookie('connect.sid');
     res.status(200).json("Logged out");
   });
-  // req.logout((err) => {
-  //   if (err) return next(err);
-  //   res.clearCookie("connect.sid");
-  //   req.session.destroy();
-  //   res.status(200).send("LOGGED OUT");
-  // });
 };
 
 module.exports = {
-  loginPage,
   registerNewUser,
   logout,
 };
