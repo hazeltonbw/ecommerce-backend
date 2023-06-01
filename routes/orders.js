68e27afeb4c9be848fa91b11876cd1e2fa987c5f
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 00:43:31 -0700

    Add Stripe API WebHook for post-order processing in DB

--- routes/orders.js
+++ routes/orders.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
-const { isLoggedIn, isAdmin } = require("../middleware/auth");
+const { isLoggedIn } = require("../middleware/auth");
+const bodyParser = require("body-parser");
 
 module.exports = (app) => {
-  app.use("/orders", isLoggedIn, router);
-  router.get("/", orderController.getOrders);
-  router.get("/:order_id", orderController.getOrderById);
-  router.put("/:order_id", orderController.updateOrderStatus);
+
+  app.use("/orders", router);
+  router.get("/", isLoggedIn, orderController.getOrders);
+  router.get("/recent", isLoggedIn, orderController.getLatestOrder)
+  /**
+   * Returns middleware that parses all bodies as a string and only looks at requests
+   * where the Content-Type header matches the type option.
+   * https://github.com/expressjs/body-parser/tree/1.19.0#bodyparserrawoptions
+   */
+  router.post("/webhook", bodyParser.raw({ type: 'application/json' }),
+    // https://stripe.com/docs/webhooks/quickstart
+    // Use Stripe webhooks for post-payment database processing
+    orderController.handleStripeEvent, orderController.createOrder)
+  router.post("/new", isLoggedIn, orderController.createOrder);
+  router.get("/:order_id", isLoggedIn, orderController.getOrderById);
+  router.put("/:order_id", isLoggedIn, orderController.updateOrderStatus);
 };
