9065568ae3c522edf9dec1f6e039fa6bcbae727c
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 00:42:34 -0700

    Change cookie maxAge var

--- config.js
+++ config.js
@@ -27,7 +27,10 @@ module.exports = {
   SESSION: {
     COOKIE: {
       secure: false,
+      // Week long cookie age
+      // 24 hours * 60 mins * 60 secs * 1000ms
       maxAge: 24 * 60 * 60 * 1000,
+      // maxAge: 10 * 1000, // max age = 10 secs
     },
     SESSION_SECRET: process.env.SESSION_SECRET,
   },
