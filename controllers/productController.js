8080a5f9279e6be43c9165fd42854146df6acf38
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-01 19:21:35 -0700

    debugging

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,6 +7,7 @@ const getProducts = async (req, res, next) => {
     const response = await productModel.getProducts(queryOpts);
     res.status(200).send(response);
   } catch (err) {
+    console.log(err)
     next(err);
   }
 };
