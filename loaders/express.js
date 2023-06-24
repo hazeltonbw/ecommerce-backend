8bf69e6d092aafc531c06b310ae4bc5870d20d8a
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-23 20:41:22 -0700

    cors testing

--- loaders/express.js
+++ loaders/express.js
@@ -68,7 +68,8 @@ module.exports = (app) => {
             secret: config.SESSION.SESSION_SECRET,
             resave: false,
             cookie: {
-                secure: 'auto',
+                //secure: 'auto',
+                secure: false,
                 // Week long cookie age
                 // 24 hours * 60 mins * 60 secs * 1000ms
                 maxAge: 7 * 24 * 60 * 60 * 1000,
