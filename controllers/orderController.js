7f8ae4db1ad3a2688e3e6b6f3340279347bfa6b8
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-25 00:56:01 -0700

    Use correct Webhook secret for Prod/Local environments

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,7 +2,7 @@ require("dotenv").config({
     path: "../.env",
 });
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
+const endpointSecret = process.env.NODE_ENV === "production" ? process.env.STRIPE_WEBHOOK_SECRET_LIVE : process.env.STRIPE_WEBHOOK_SECRET_LOCAL;
 const { getCartIdByUserId } = require("../models/cart");
 const orderModel = require("../models/order");
 
