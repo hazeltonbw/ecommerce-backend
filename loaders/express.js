ce4a20dd084afe7ee3682d2338587777d3cd5a25
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-30 22:50:41 -0700

    cookies

--- loaders/express.js
+++ loaders/express.js
@@ -57,8 +57,6 @@ module.exports = (app) => {
       cookie: {
         secure: process.env.NODE_ENV === "production",
         // Week long cookie age
-        httpOnly: false,
-        domain: ".onrender.com",
         maxAge: 7 * 24 * 60 * 60 * 1000,
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       },
