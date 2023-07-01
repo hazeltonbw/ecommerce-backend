89c9f2548676f5b4b9e898626495af1d8e7286cf
Brandon Hazelton <hazeltonbw@gmail.com>
2023-06-30 22:36:42 -0700

    i want a cookie

--- loaders/express.js
+++ loaders/express.js
@@ -60,7 +60,7 @@ module.exports = (app) => {
         httpOnly: false,
         domain: ".onrender.com",
         maxAge: 7 * 24 * 60 * 60 * 1000,
-        sameSite: "strict",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       },
       saveUninitialized: false,
       proxy: process.env.NODE_ENV === "production",
