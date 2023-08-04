cdfd1b8af377939a85675c67d844065da13b5518
Brandon Hazelton <hazeltonbw@gmail.com>
2023-08-03 19:39:04 -0700

    Serve up the front-end files through the dist/ folder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const app = express();
-const loaders = require("./loaders");
+const express = require('express')
+const app = express()
+const loaders = require('./loaders')
 
+const path = require('path')
 // Init application loaders
-loaders(app);
+app.use(express.static(path.join(__dirname, '/dist')))
+
+// app.use(express.static('dist'))
+loaders(app)
 
 // exported for Jest testing
-module.exports = app;
+module.exports = app
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const app = require("./index");
-require("dotenv").config({
-  path: "../.env",
-});
+const app = require('./index')
+require('dotenv').config({
+  path: '../.env',
+})
 
-const { PORT } = require("./config");
+const { PORT } = require('./config')
 
-app.get("/", (req, res, next) => {
-  if (req.user) {
-    res.status(200).json(req.user);
-  } else {
-    res.status(200).json("The root says you're not logged in! :)");
-  }
-});
+// app.get('/', (req, res, next) => {
+//   if (req.user) {
+//     res.status(200).json(req.user)
+//   } else {
+//     res.status(200).json("The root says you're not logged in! :)")
+//   }
+// })
 
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`);
-});
+  console.log(`Server listening on PORT ${PORT}`)
+})
