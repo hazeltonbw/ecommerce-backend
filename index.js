d4dafc3b84b523a467ff922ea3ba899e760033eb
Brandon Hazelton <hazeltonbw@gmail.com>
2023-08-04 01:27:35 -0700

    Serve static files necessary for production

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const loaders = require('./loaders')
 
 const path = require('path')
 // Init application loaders
-// app.use(express.static(path.join(__dirname, '/dist')))
+app.use(express.static(path.join(__dirname, '/dist')))
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
