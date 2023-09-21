e3e5743ed095f9d8f4b65e65f8558597ddc57015
Brandon Hazelton <hazeltonbw@gmail.com>
2023-09-21 10:01:01 +0000

    Add dependencies

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require('express')
 const app = express()
 const loaders = require('./loaders')
-
 const path = require('path')
-// Init application loaders
 
-// app.use(express.static('dist'))
+// Init application loaders
 loaders(app)
 app.use(express.static(path.join(__dirname, '/dist')))
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
-
 
 // exported for Jest testing
 module.exports = app
