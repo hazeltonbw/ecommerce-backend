5c2a6ab6365d001de58a3a366281f535b086608b
Brandon Hazelton <hazeltonbw@gmail.com>
2023-09-21 10:00:25 +0000

    Use raw body data for Stripe webhook

--- routes/orders.js
+++ routes/orders.js
@@ -5,23 +5,29 @@ const { isLoggedIn } = require('../middleware/auth')
 const bodyParser = require('body-parser')
 
 module.exports = (app) => {
-  app.use('/api/orders', router)
-  router.get('/', isLoggedIn, orderController.getOrders)
-  router.get('/recent', isLoggedIn, orderController.getLatestOrder)
-  /**
-   * Returns middleware that parses all bodies as a string and only looks at requests
-   * where the Content-Type header matches the type option.
-   * https://github.com/expressjs/body-parser/tree/1.19.0#bodyparserrawoptions
-   */
-  //router.post("/webhook", express.raw({ type: 'application/json' }),
-  router.post(
-    '/webhook',
-    // https://stripe.com/docs/webhooks/quickstart
-    // Use Stripe webhooks for post-payment database processing
-    orderController.handleStripeEvent,
-    orderController.createOrder
-  )
-  router.post('/new', isLoggedIn, orderController.createOrder)
-  router.get('/:order_id', isLoggedIn, orderController.getOrderById)
-  router.put('/:order_id', isLoggedIn, orderController.updateOrderStatus)
+	app.use('/api/orders', router)
+	router.get('/', isLoggedIn, orderController.getOrders)
+	router.get('/recent', isLoggedIn, orderController.getLatestOrder)
+	/**
+	 * Returns middleware that parses all bodies as a string and only looks at requests
+	 * where the Content-Type header matches the type option.
+	 * https://github.com/expressjs/body-parser/tree/1.19.0#bodyparserrawoptions
+	 */
+	//router.post("/webhook", express.raw({ type: 'application/json' }),
+	//router.post('/webhook',
+	// https://stripe.com/docs/webhooks/quickstart
+	// Use Stripe webhooks for post-payment database processing
+	// orderController.handleStripeEvent,
+	//orderController.createOrder
+	//)
+	router.post("/webhook", express.raw({ type: 'application/json' }),
+	//router.post('/webhook',
+		// https://stripe.com/docs/webhooks/quickstart
+		// Use Stripe webhooks for post-payment database processing
+		orderController.handleStripeEvent,
+		orderController.createOrder
+	)
+	router.post('/new', isLoggedIn, orderController.createOrder)
+	router.get('/:order_id', isLoggedIn, orderController.getOrderById)
+	router.put('/:order_id', isLoggedIn, orderController.updateOrderStatus)
 }
